4ff5219d642785618f0d02fa917a3a063f40f4eb
AnimalToken <100366916+AnimalToken-io@users.noreply.github.com>
2022-06-02 11:12:42 -0400

    Update AnimalToken_ERC20.sol

--- AnimalToken_ERC20.sol
+++ AnimalToken_ERC20.sol
@@ -84,7 +84,7 @@ interface IERC20 {
 // File: contracts\open-zeppelin-contracts\math\SafeMath.sol
 
 //pragma solidity ^0.5.0;
-pragma solidity >=0.5.0 <0.8.4;
+pragma solidity 0.8.4;
 
 /**
  * @dev Wrappers over Solidity's arithmetic operations with added overflow
@@ -195,7 +195,7 @@ library SafeMath {
 // File: contracts\open-zeppelin-contracts\token\ERC20\ERC20.sol
 
 //pragma solidity ^0.5.0;
-pragma solidity >=0.5.0 <0.8.4;
+pragma solidity 0.8.4;
 
 
 
@@ -426,7 +426,7 @@ contract ERC20 is IERC20 {
 // File: contracts\ERC20\TokenMintERC20Token.sol
 
 //pragma solidity ^0.5.0;
-pragma solidity >=0.5.0 <0.8.4;
+pragma solidity 0.8.4;
 
 
 /**
